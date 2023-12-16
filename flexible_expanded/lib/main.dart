7dbd68a568820f9479cebeb44395cc420632ddab
gedeastu <gede.astu@gmail.com>
2023-12-16 11:51:24 +0700

    done

--- flexible_expanded/lib/main.dart
+++ flexible_expanded/lib/main.dart
@@ -23,6 +23,66 @@ class FlexibleExpanded extends StatefulWidget {
 class _FlexibleExpandedState extends State<FlexibleExpanded> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flexible vs Expanded",style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.amber,
+      ),
+      body: 
+      Container(
+        height: double.infinity,
+        child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+        Flexible(
+          fit: FlexFit.tight,
+          flex:150,
+          child: Row(
+            children: [
+              Container(
+                height: 200.0,
+                color: Colors.amber,
+                child: Text("Text 1"),
+              ),
+              Container(
+                height: 200.0,
+                color: Colors.amber,
+                child: Text("Text 1"),
+              ),
+              Container(
+                height: 200.0,
+                color: Colors.amber,
+                child: Text("Text 1"),
+              ),
+              Container(
+                height: 200.0,
+                color: Colors.amber,
+                child: Text("Text 1"),
+              )
+            ],
+          ),
+        ),
+        Flexible(
+          fit: FlexFit.tight,
+          flex: 100,
+          child: Container(
+            height: 200.0,
+            color: Colors.red,
+            child: Text("Text 2"),
+          ),
+        ),
+        Expanded(
+          //fit: FlexFit.tight,
+          flex: 50,
+          child: Container(
+            height: 200.0,
+            color: Colors.blueGrey,
+            child: Text("Text 3"),
+          ),
+        )
+      ]),
+      )
+    );
   }
 }
